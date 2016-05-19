df0bc6f63e38707ae6c725dd5536f8f75e2ba9fd
takaaki.konaka <takaaki.konaka@gaiax.com>
2016-05-19 14:39:45 +0900

    無限ループを続けるかの判定部分をリファクタ

--- main.pl
+++ main.pl
@@ -6,9 +6,8 @@ use FizzBuzz;
 use TemporaryOperations;
 use FileOperations;
 
-my $state;
 
-do{
+while(1){
     print 'please input a number (0 ~ 4) :';
     my $mode = <STDIN>;
     chomp($mode);
@@ -28,33 +27,30 @@ do{
 
         #入力値とその結果を一時的に格納する
         TemporaryOperations->save($number,$result);
-        $state =1;
     
     }elsif($mode eq "2"){
         #fizzbuzzの結果を表示する
         TemporaryOperations->show();
-        $state =1;
     
     }elsif($mode eq "3"){
         #fizzbuzzの結果をファイルに格納する
         my $result = TemporaryOperations->get_result();
         FileOperations->write($result);
-        $state =1;
 
     }elsif($mode eq "4"){
         #fizzbuzzのファイルの内容を表示する
         FileOperations->read();
-        $state =1;
     
     }elsif($mode eq "0"){
         print "stop program";
         #プログラムを終了する
-        $state =0;
+        exit(0);
     
     }else{
         print "input : illegal\n";
         #想定しない入力。プログラムは継続させる
-        $state =1;
     }
 
-}while($state)
+};
+
+
